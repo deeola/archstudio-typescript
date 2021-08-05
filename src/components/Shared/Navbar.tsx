d41cde94164bbd7f776111943a2003c08c8f21ee
Adeola Bamigboye <clazikar@gmail.com>
2021-08-05 11:56:40 +0200

    delete neccessary files

---
+++ src/components/Shared/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../assets/logo.svg";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
