a47c1afa42b44e4069017921b382427eea236c03
Adeola Bamigboye <clazikar@gmail.com>
2021-08-05 16:14:04 +0200

    Navbar component added

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -1,26 +1,41 @@
 import React from "react";
 import logo from "../../assets/logo.svg";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import hamburger from "../../assets/icons/icon-hamburger.svg";
+import close from "../../assets/icons/icon-close.svg";
 
 type Props = {};
 
 const Navbar: React.FC<Props> = () => {
-  // const {
-  //     displayMenu,
-  //     closeMenu,
-  //     ulDisplay,
-  //     DisplayCloseIcon,
-  //     DisplayOpenIcon,
-  //   } = ArchContext;
-
   return (
     <nav>
       <div className="logo">
-        <Link>
+        <div>
           <img className="mainLogo" src={logo} alt="logo"></img>
-        </Link>
+        </div>
       </div>
-      <div></div>
+      <div className="HambugerClose">
+        <img className="hamIcon" src={hamburger} alt="ham-icon"></img>
+        <img className="closeIcon" src={close} alt="close-icon"></img>
+      </div>
+      <ul className="navUl">
+        <li>
+          {" "}
+          <NavLink activeClassName="actives" className="myLink" to="/Portfolio">
+            Portfolio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="actives" className="myLink" to="/About">
+            About Us
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="actives" className="myLink" to="/Contact">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
