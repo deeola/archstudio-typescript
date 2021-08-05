ee72af2850e8b20d64e1c57fbc690f7b8d8a782d
Adeola Bamigboye <clazikar@gmail.com>
2021-08-05 13:02:59 +0200

    add dependecy and update folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./css/style.css";
+import Home from "./components/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" render={(props) => <Home />} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
